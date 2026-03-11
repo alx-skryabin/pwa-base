c6ea36ecac252f7617d04dc0d6808cefee53ae37
A.Skriabin <A.Skriabin@asntl.ru>
2026-03-11 18:19:02 +0300

    in work

--- src/entities/guide/model/guideActions.ts
+++ src/entities/guide/model/guideActions.ts
@@ -1,19 +1,18 @@
 import { runWithDb, getAll, getAllByIndex } from '@shared/libs/indexedDb'
 import type { Continent, Country } from './types'
 import { CONTINENT_ID_IDX } from '@shared/libs/indexedDb/storeIndexes.ts'
+import { CONTINENTS_STORE, COUNTRIES_STORE } from '@shared/libs/indexedDb/storeNames.ts'
 
 /**
  * Загружает все континенты из IndexedDB.
  */
 export async function getContinents(): Promise<Continent[]> {
-  console.log('getContinents')
-  return runWithDb(db => getAll<Continent>(db, 'continents'))
+  return runWithDb(db => getAll<Continent>(db, CONTINENTS_STORE))
 }
 
 /**
  * Загружает страны континента по индексу continentId (без перебора всего store).
  */
 export async function getCountriesByContinent(continentId: number): Promise<Country[]> {
-  console.log('getCountriesByContinent')
-  return runWithDb(db => getAllByIndex<Country>(db, 'countries', CONTINENT_ID_IDX, continentId))
+  return runWithDb(db => getAllByIndex<Country>(db, COUNTRIES_STORE, CONTINENT_ID_IDX, continentId))
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useRegionRusWithMagnet } from '@entities/user'
+import { RegionsRusList } from '@widgets/regions-rus-list'
+import { Typography } from 'antd'
+
+const { Title } = Typography
 
 const Home: React.FC = () => {
-  return <div>Home</div>
+  const { data, isLoading } = useRegionRusWithMagnet()
+
+  return (
+    <div className="home-page">
+      <Title level={4} style={{ marginBottom: 16 }}>
+        Регионы России
+      </Title>
+      <RegionsRusList regions={data} loading={isLoading} />
+    </div>
+  )
 }
 
 export default Home
